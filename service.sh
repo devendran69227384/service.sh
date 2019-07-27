9b3020104d5ce31a3821e76ccd3ecc62e772b9d8
DEVENDRAN.M <devendrandevendran052@gmail.com>
2019-07-27 23:05:53 +0530

    Add files via upload

---
+++ service.sh
@@ -0,0 +1,7 @@
+echo "DEVA HACKER"
+/etc/init.d/bluetooth restart
+/etc/init.d/network-manager restart
+/etc/init.d/postgresql start
+/etc/init.d/apache2 start
+/etc/init.d/ssh start
+/etc/init.d/vsftpd start
